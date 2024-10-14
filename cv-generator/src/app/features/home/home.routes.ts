5b9db15de31074ccf09f47e64f72ddd6ec9e79b8
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-10-14 16:44:07 +0300

    feat: create employees, project, resume components and routes for them

--- cv-generator/src/app/features/home/home.routes.ts
+++ cv-generator/src/app/features/home/home.routes.ts
@@ -1,10 +1,26 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
 export const homeRoutes: Route[] = [
   {
     path: '',
     component: HomeComponent,
-    children: [],
+    children: [
+      {
+        path: 'employees',
+        component: EmployeesComponent,
+      },
+      {
+        path: 'projects',
+        component: ProjectsComponent,
+      },
+      {
+        path: 'resume',
+        component: ResumeComponent,
+      },
+    ],
   },
 ];
