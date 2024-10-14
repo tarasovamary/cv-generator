c72adc5cd51825320d8364808012f390df911e8f
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-10-14 13:06:04 +0300

    feat: create home and auth components with routes

--- cv-generator/src/app/app.routes.ts
+++ cv-generator/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./features/home/home.routes').then((r) => r.homeRoutes) },
+  { path: 'auth', component: AuthComponent },
+];
