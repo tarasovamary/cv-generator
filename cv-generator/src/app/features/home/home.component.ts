33ef1aac4b116ae41ea5782294476b6ac4bb4ca8
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-10-14 16:04:41 +0300

    feat: implement sidebar component

--- cv-generator/src/app/features/home/home.component.ts
+++ cv-generator/src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
