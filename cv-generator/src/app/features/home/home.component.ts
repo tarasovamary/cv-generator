cbf8b9873cf6838b7237bac31e9b7faf0d230f38
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-10-14 15:09:40 +0300

    feat: implement header component

--- cv-generator/src/app/features/home/home.component.ts
+++ cv-generator/src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
