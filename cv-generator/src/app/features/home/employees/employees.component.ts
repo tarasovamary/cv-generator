93cc0fd975c817822e998f4acd204ce9bba17fc9
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-10-24 11:53:56 +0300

    feat: add employees list

--- cv-generator/src/app/features/home/employees/employees.component.ts
+++ cv-generator/src/app/features/home/employees/employees.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ButtonModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
-export class EmployeesComponent {}
+export class EmployeesComponent implements OnInit {
+  employees: any[] = [];
+
+  ngOnInit() {
+    //TODO: Replace this with actual employee data fetching logic
+    this.employees = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@example.com',
+        department: 'HR',
+        specialization: 'Recruiting',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        email: 'jane.smith@example.com',
+        department: 'Development',
+        specialization: 'Frontend',
+      },
+    ];
+  }
+}
