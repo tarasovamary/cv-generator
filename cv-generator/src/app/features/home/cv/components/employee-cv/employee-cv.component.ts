f8ed480ac4b86808099c98f63d017b78e6f3c4bc
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-11-05 15:29:12 +0300

    feat: implement logic for selecting the active CV from the list

--- cv-generator/src/app/features/home/cv/components/employee-cv/employee-cv.component.ts
+++ cv-generator/src/app/features/home/cv/components/employee-cv/employee-cv.component.ts
@@ -6,17 +6,18 @@ import { Observable } from 'rxjs';
 import { CV } from '../../models/cv.model';
 import * as CvActions from '../../store/cv.actions';
 import { selectAllCv } from '../../store/cv.selectors';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee-cv',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass, FormsModule, ReactiveFormsModule, AsyncPipe],
+  imports: [NgIf, NgFor, NgClass, FormsModule, ReactiveFormsModule, AsyncPipe, RouterLink],
   templateUrl: './employee-cv.component.html',
   styleUrl: './employee-cv.component.scss',
 })
 export class EmployeeCvComponent {
   @Input({ required: true }) employeeId!: string;
-  cvList: CV[] = [];
+  activeCvId!: string;
   cvForm!: UntypedFormGroup;
   cvs$!: Observable<CV[]>;
 
@@ -29,10 +30,8 @@ export class EmployeeCvComponent {
     this.store.dispatch(CvActions.getAllCv({ employeeId: this.employeeId }));
     this.cvs$ = this.store.select(selectAllCv);
 
-    this.cvList = [];
-
     this.cvForm = this.fb.group({
-      cvName: ['', Validators.required],
+      name: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -41,6 +40,13 @@ export class EmployeeCvComponent {
       skills: ['', Validators.required],
       description: [''],
     });
+
+    // Set the first CV as active
+    this.cvs$.subscribe((cvs) => {
+      if (cvs.length > 0) {
+        this.setActiveCv(cvs[0]);
+      }
+    });
   }
 
   onDeleteCv(id: string) {}
@@ -48,4 +54,10 @@ export class EmployeeCvComponent {
   onSubmit() {}
 
   onCancel() {}
+
+  setActiveCv(cv: CV) {
+    this.activeCvId = cv._id;
+
+    this.cvForm.patchValue(cv);
+  }
 }
