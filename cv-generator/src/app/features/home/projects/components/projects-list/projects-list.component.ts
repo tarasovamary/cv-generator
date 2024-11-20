6503a3f33f39bdea79845ab2fe63c2c05c9f9d06
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-11-20 16:17:40 +0300

    feat: implement template for projectsListComponent

--- cv-generator/src/app/features/home/projects/components/projects-list/projects-list.component.ts
+++ cv-generator/src/app/features/home/projects/components/projects-list/projects-list.component.ts
@@ -1,10 +1,25 @@
+import { AsyncPipe, DatePipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { Observable } from 'rxjs';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-projects-list',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ButtonModule, AsyncPipe, NgIf, RouterLink, ConfirmDialogModule, ToastModule, DatePipe],
   templateUrl: './projects-list.component.html',
   styleUrl: './projects-list.component.scss',
 })
-export class ProjectsListComponent {}
+export class ProjectsListComponent {
+  projects$: Observable<Project[]>;
+
+  constructor() {}
+
+  onSelect(id: string) {}
+  onDelete(id: string, event: MouseEvent) {}
+}
