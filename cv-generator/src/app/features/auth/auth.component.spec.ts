fe402c324587c3230b64eb226f19c2114b4b6421
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-12-04 11:28:38 +0300

    feat: add tests for auth component

--- cv-generator/src/app/features/auth/auth.component.spec.ts
+++ cv-generator/src/app/features/auth/auth.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AuthComponent } from './auth.component';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideRouter } from '@angular/router';
 import { Actions } from '@ngrx/effects';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { provideRouter } from '@angular/router';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import * as AuthActions from '../auth/store/auth.actions';
+import { AuthComponent } from './auth.component';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
   let mockStore: MockStore;
   let mockActions: Actions;
+  let dispatchSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,10 +23,48 @@ describe('AuthComponent', () => {
     component = fixture.componentInstance;
     mockStore = TestBed.inject(MockStore);
     mockActions = TestBed.inject(Actions);
+    dispatchSpy = spyOn(mockStore, 'dispatch').and.callThrough();
+
     fixture.detectChanges();
   });
 
   it('should create AuthComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch signup action if form is valid', () => {
+    component.isSignup = true;
+
+    component.loginForm.setValue({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+
+    component.onSubmit();
+
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      AuthActions.signup({
+        email: 'test@example.com',
+        password: 'password123',
+      }),
+    );
+  });
+
+  it('should dispatch login action if form is valid', () => {
+    component.isSignup = false;
+
+    component.loginForm.setValue({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+
+    component.onSubmit();
+
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      AuthActions.login({
+        email: 'test@example.com',
+        password: 'password123',
+      }),
+    );
   });
 });
