2715e8b936fb0b8a12e0961f87345fdc250fcec7
Mariya Kryvitskaya <masha.oath@gmail.com>
2025-01-09 12:52:36 +0300

    feat: add tests for CvListComponent

--- cv-generator/src/app/features/layout/cv/components/cv-list/cv-list.component.spec.ts
+++ cv-generator/src/app/features/layout/cv/components/cv-list/cv-list.component.spec.ts
@@ -1,20 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CvListComponent } from './cv-list.component';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { ActivatedRoute, Router, convertToParamMap, provideRouter } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { of } from 'rxjs';
+import { CvListComponent } from './cv-list.component';
 
 describe('CvListComponent', () => {
   let component: CvListComponent;
   let fixture: ComponentFixture<CvListComponent>;
   let mockStore: MockStore;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let mouseEventSpy: jasmine.SpyObj<MouseEvent>;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
-    const activatedRoute = {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    mouseEventSpy = jasmine.createSpyObj('MouseEvent', ['stopPropagation']);
+
+    const activatedRouteMock = {
       parent: {
-        snapshot: {
-          paramMap: convertToParamMap({ id: '123' }),
-        },
+        snapshot: {},
+        params: of({}),
       },
     };
 
@@ -22,20 +28,35 @@ describe('CvListComponent', () => {
       imports: [CvListComponent],
       providers: [
         provideMockStore(),
-        provideRouter([]),
-        { provide: ActivatedRoute, useValue: activatedRoute },
-        ConfirmationService,
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
         MessageService,
+        ConfirmationService,
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CvListComponent);
     component = fixture.componentInstance;
     mockStore = TestBed.inject(MockStore);
+    activatedRoute = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
   it('should create CvListComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate correctly when onSelect is called', () => {
+    const testId = '123';
+
+    component.onSelect(testId);
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['../', testId], { relativeTo: activatedRoute });
+  });
+
+  it('should call stopPropagation when onDelete is called', () => {
+    component.onDelete('123', mouseEventSpy);
+
+    expect(mouseEventSpy.stopPropagation).toHaveBeenCalled();
   });
 });
